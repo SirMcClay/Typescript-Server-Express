b26272bf2b4bd4fed0f994ab201760b33a5c936c
sirmcclay <mcclay.design@gmail.com>
2020-12-03 12:17:55 -0300

    Feat: Interface for PropertyDescriptor to shown an error for unmatchable routes

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -3,6 +3,12 @@ import { get, controller, post, bodyValidator } from './decorators';
 
 @controller('/auth')
 class LoginController {
+	// PropertyDescriptor shown an error because mismatch types
+	// @get('/')
+	// add(a: number, b: number): number {
+	// 	return a + b;
+	// }
+
 	@get('/login')
 	getLogin(req: Request, res: Response): void {
 		res.send(`
